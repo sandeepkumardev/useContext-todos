b72b5d17b123f60ef32872768b691a0a9c5cfb9d
sandeep kumar <sandeepypb@gmail.com>
2024-11-25 20:12:19 +0530

    ✨ Feat: create and store todo

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 const Todo = () => {
-  return <div>Todo page</div>;
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <h1>Todo Page</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
